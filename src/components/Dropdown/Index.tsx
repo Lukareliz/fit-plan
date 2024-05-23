c665bebea2dc5117a0087c9a2dc830be2ede5337
= <lyulucca@gmail.com>
2024-05-23 19:15:06 -0300

    feat: Dropdown component and exerciseList.json created

---
+++ src/components/Dropdown/Index.tsx
@@ -0,0 +1,10 @@
+import { AiOutlineCaretUp, AiOutlineCaretDown } from "react-icons/ai"
+import ExerciseList from '../../exerciseList.json' 
+
+const Dropdown = () => {
+  return (
+    <div>Dropdown</div>
+  )
+}
+
+export default Dropdown
