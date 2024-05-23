cf9757b74fd17df10cdc37629254969204256941
= <lyulucca@gmail.com>
2024-05-23 17:09:01 -0300

    fix: initial state value changed(navigation)

--- src/components/Navigation/Index.tsx
+++ src/components/Navigation/Index.tsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom"
 import { useState } from "react"
 
 const Navigation = () => {
-    const [selected, setSelected] = useState<string>('');
+    const [selected, setSelected] = useState<string>('link1');
 
     return (
         <nav className="bg-gray flex h-28 justify-center items-center gap-20 xl:gap-80">
-            <Link to={"/"} className={`text-white text-xl font-bold ${selected === 'link1' ? 'border-b-2 border-fit-green' : 'opacity-50'}`} onClick={() => setSelected('link1')}>Treinos</Link>
-            <Link to={"/exercises"} className={`text-white text-xl font-bold ${selected === 'link2' ? 'border-b-2 border-fit-green' : 'opacity-50'}`} onClick={() => setSelected('link2')}>Exercícios</Link>
+            <Link to={"/"} className={`text-white text-xl font-bold ${selected === 'link1' ? 'border-b-4 border-fit-green' : 'opacity-50'}`} onClick={() => setSelected('link1')}>Treinos</Link>
+            <Link to={"/exercises"} className={`text-white text-xl font-bold ${selected === 'link2' ? 'border-b-4 border-fit-green' : 'opacity-50'}`} onClick={() => setSelected('link2')}>Exercícios</Link>
         </nav>
     )
 }
