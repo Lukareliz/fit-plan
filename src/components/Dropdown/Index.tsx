1fc4325aee49d8f4dbd0e7baf8ef9d251022b25e
= <lyulucca@gmail.com>
2024-05-24 00:31:02 -0300

    feat: dropdown menu body

--- src/components/Dropdown/Index.tsx
+++ src/components/Dropdown/Index.tsx
@@ -1,10 +1,41 @@
-import { AiOutlineCaretUp, AiOutlineCaretDown } from "react-icons/ai"
-import ExerciseList from '../../exerciseList.json' 
+import { AiOutlineCaretRight, AiOutlineCaretDown } from "react-icons/ai"
+import exercise from '../../exerciseList.json'
+import { useState } from "react";
 
-const Dropdown = () => {
-  return (
-    <div>Dropdown</div>
-  )
+interface DropdownProps {
+    day: string;
+}
+
+interface exercise {
+    name: string;
+    muscle_group: string;
+    equipment: string;
+}
+
+const Dropdown: React.FC<DropdownProps> = ({ day }) => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    return (
+        <div className="w-full flex justify-center">
+            <div className="relative flex flex-col items-center w-[90%] lg:w-[50%] h-[300px] rounded-xl">
+                <button onClick={() => setIsOpen((prev) => !prev)} className="bg-black text-white p-4 w-full flex items-center justify-between font-bold text-lg rounded-full tracking-wider">
+                    {day}
+                    {!isOpen ? (<AiOutlineCaretRight className="h-8"/>):(<AiOutlineCaretDown className="h-8 text-fit-green"/>)}
+                </button>
+                {isOpen && (
+                    <div className="bg-black absolute top-20 flex gap-5 flex-col items-start rounded-xl p-2 w-full">
+                        {/* Esta estrutura no futuro irá mudar para recever a escolha dinâmica dos exercícios. */}
+                        {exercise.map((item, i) => (
+                            <div className="flex w-full justify-between " key={i}>
+                                <h3 className="text-white">{item.name}</h3>
+                                <input type="checkbox" name="check-exercise"/>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default Dropdown
--- src/routes/Workouts/Index.tsx
+++ src/routes/Workouts/Index.tsx
@@ -1,12 +1,17 @@
 import Header from "../../components/Header/Index"
 import Navigation from "../../components/Navigation/Index"
+import Dropdown from "../../components/Dropdown/Index"
 
 const Workouts = () => {
   return (
-    <main className="bg-gray h-80">
+    <main className="bg-gray h-full">
       <Header />
       <Navigation />
       <h4 className="text-white text-center text-lg font-light pt-6">Seus dias de treino.</h4>
+      <section className="mt-6 flex flex-col">
+        <Dropdown day="Segunda-feira" />
+        <Dropdown day="Terça-feira" />
+      </section>
     </main>
   )
 }
